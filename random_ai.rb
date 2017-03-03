4293753d952dc811083cf27805a9b15e886c4b0b
Brian Lewis <blewis8958@gmail.com>
2017-03-03 11:31:41 -0500

    added tests for picking a random spot along with picking a full spot

--- random_ai.rb
+++ random_ai.rb
@@ -1,7 +1,17 @@
-class Random
-	attr_reader :create, :counter
+class Random_ai
+	attr_accessor :create, :counter
 	def initialize
 		@create = ["X", "X", "X", "X"]
-		@counter = 0
+		@counter = rand(0..8)
+	end
+
+	def pick_spot(board)
+		while board.check_position?(@counter) == false
+			@counter = rand(0..8)
+		end
+
+		board.set_position(@counter, @create.pop)
+
+		return board
 	end
 end
--- test_random_ai.rb
+++ test_random_ai.rb
@@ -4,15 +4,27 @@ require_relative "board.rb"
 
 class TestRandomAI < Minitest::Test
 	def test_new_ai
-		ai1 = Random.new
+		ai1 = Random_ai.new
 		result = ["X", "X", "X", "X"]
 		assert_equal(result, ai1.create)
 	end
 
 	def test_pick_spot
 		board = Board.new
-		ai1 = Random.new
-		result = board.setup.count("X")
-		assert_equal()
+		ai1 = Random_ai.new
+		result = true
+		ai1.pick_spot(board)
+		assert_equal(result , board.setup.include?("X"))
+	end
+
+	def test_pick_full_spot
+		board = Board.new
+		ai1 = Random_ai.new
+		board.set_position(0, "O")
+		ai1.counter = 0
+		ai1.pick_spot(board)
+		result = true
+		print board.setup
+		assert_equal(result, board.setup.include?("X"))
 	end
 end
