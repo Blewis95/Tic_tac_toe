64d5aa3d3ea4a355878b692a1657557591a27bcf
Brian Lewis <blewis8958@gmail.com>
2017-03-09 12:55:32 -0500

    trying to get another test to work but running into problems

--- test_unbeatable_ai.rb
+++ test_unbeatable_ai.rb
@@ -8,4 +8,12 @@ class TestUnbeatableAI < Minitest::Test
 		result = ["X", "X", "X", "X"]
 		assert_equal(result, ai1.create)
 	end
+
+	def test_pick_spot_step_1
+		board = Board.new
+		ai1 = Unbeatable_ai.new
+		board.setup = ["X", "X", "", "", "", "", "", "", ""]
+		result = ["X", "X", "X", "", "", "", "", "", ""]
+		assert_equal(result, ai1.pick_spot(board))
+	end
 end
--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -1,6 +1,41 @@
 class Unbeatable_ai
-	attr_accessor :create
+	attr_accessor :create, :counter
 	def initialize
 		@create = ["X", "X", "X", "X"]
+		@counter = 0
+	end
+
+	def pick_spot(board)
+
+		check_win?(board)
+
+		if board.check_position?(@counter) == true
+			board.set_position(@counter, @create.pop)
+		end
+
+		board.setup
+
+	end
+
+	def check_win?(board)
+		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+		counter = 0
+
+
+		winning.each do |option|
+			option.each do |spot|
+				if board.setup[spot] == "X"
+					counter += 1
+				end
+			end
+
+			if counter == 2
+				option.each do |spot|
+					if (board.setup[spot] == "")
+						@counter = spot
+					end
+				end
+			end
+		end
 	end
 end
