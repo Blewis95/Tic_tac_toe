02f8a0421c1717287c0cd0bb37bf9fad1a4e5edf
Brian Lewis <blewis8958@gmail.com>
2017-03-10 13:28:04 -0500

    added a test to check if the changing of turns works

--- game.rb
+++ game.rb
@@ -1,1 +1,25 @@
-game
+require_relative "board.rb"
+require_relative "random_ai.rb"
+require_relative "sequential_ai.rb"
+
+class Game
+	attr_reader :turn
+	def initialize
+		@turn == true
+	end
+
+	def change_markers(player2)
+		player2.create = ["O", "O", "O", "O"]
+	end
+
+	def change_turn(player1, player2)
+		if player1.turn == true || player2.turn == false
+			player1.turn = false
+			player2.turn = true
+		elsif player2.turn == true || player1.turn == false
+			player1.turn = true
+			player2.turn = false
+		end
+	end
+
+end
--- test_game.rb
+++ test_game.rb
@@ -2,12 +2,31 @@ require "minitest/autorun"
 require_relative "board.rb"
 require_relative "sequential_ai.rb"
 require_relative "random_ai.rb"
+require_relative "game.rb"
 
 class TestGame < Minitest::Test
 	def test_new_game
+		game = Game.new
 		board = Board.new
 		player1 = Random_ai.new
 		player2 = Random_ai.new
+		player1.turn = game.turn
+		game.change_markers(player2)
 		assert_equal(["X", "X", "X", "X", "X"],player1.create)
+		assert_equal(["O","O","O","O"],player2.create)
+	end
+
+	def test_change_turn
+		game = Game.new
+		board = Board.new
+		player1 = Random_ai.new
+		player2 = Random_ai.new
+		player1.turn = game.turn
+		player2.turn = false
+		game.change_markers(player2)
+		game.change_turn(player1,player2)
+
+		assert_equal(true, player2.turn)
+		assert_equal(false, player1.turn)
 	end
 end
