d7b84c1feb273f8cc69534c322e6479a9f701b27
Brian Lewis <blewis8958@gmail.com>
2017-03-15 12:44:17 -0400

    edited to add the player class

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative "board.rb"
 require_relative "random_ai.rb"
 require_relative "sequential_ai.rb"
 require_relative "game.rb"
+require_relative "player.rb"
 
 board = Board.new
 game = Game.new
@@ -9,7 +10,7 @@ ai1 = String
 ai2 = String
 
 puts "Welcome to Tic Tac Toe"
-puts "Player types: Sequential = S, Random = R"
+puts "Player types: Sequential = S, Random = R, Player = P"
 
 begin
 	print "Please enter AI type for player 1: "
@@ -19,12 +20,12 @@ begin
 		player1 = Sequential.new
 	elsif ai1.upcase == "R"
 		player1 = Random_ai.new
-	# elsif ai1.upcase == "P"
-	# 	ai1 = Player.new
+	elsif ai1.upcase == "P"
+		player1 = Player.new
 	else
 		puts "Error, incorrect input"
 	end
-end while (ai1.upcase != "S") && (ai1.upcase != "R")
+end while (ai1.upcase != "S") && (ai1.upcase != "R") && (ai1.upcase != "P")
 
 
 begin
@@ -35,12 +36,12 @@ begin
 		player2 = Sequential.new
 	elsif ai2.upcase == "R"
 		player2 = Random_ai.new
-	# elsif ai2.upcase == "P"
-	# 	ai2 = Player.new
+	elsif ai2.upcase == "P"
+		player2 = Player.new
 	else
 		puts "Error, incorrect input"
 	end
-end while (ai1.upcase != "S") && (ai1.upcase != "R")
+end while (ai1.upcase != "S") && (ai1.upcase != "R") && (ai1.upcase != "P")
 
 # puts player1
 # puts player2
