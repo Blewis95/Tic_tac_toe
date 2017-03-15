6ac6bdaa6295e76e4e10ed59cbbfac0d24f5fc51
Brian Lewis <blewis8958@gmail.com>
2017-03-15 12:39:45 -0400

    added a method for picking a spot on the board

--- player.rb
+++ player.rb
@@ -2,7 +2,27 @@ class Player
 	attr_accessor :create, :counter, :turn
 	def initialize
 		@create = ["X", "X", "X", "X", "X"]
-		@counter = 0
+		@counter = nil
 		@turn = false
 	end
+
+	def pick_spot(board)
+		begin
+			puts "Please choose a spot between 0 and 8"
+			@counter = gets.chomp
+
+			while board.check_position?(@counter.to_i) == false
+				puts "Position is full! please try again:"
+				@counter = gets.chomp
+			end 
+
+		end while ((@counter.to_i != 0) && (@counter.to_i != 1) && (@counter.to_i != 2) && (@counter.to_i != 3) && (@counter.to_i != 4) && (@counter.to_i != 5) && (@counter.to_i != 6) && (@counter.to_i != 7) && (@counter.to_i != 8))
+
+		if @turn == true
+			board.set_position(@counter.to_i, @create.pop)
+		end
+
+		return board
+	end
+
 end
--- test_player.rb
+++ test_player.rb
@@ -8,4 +8,13 @@ class TestPlayer < Minitest::Test
 		result = ["X", "X", "X", "X", "X"]
 		assert_equal(result, player1.create)
 	end
+
+	def test_pick_spot
+		board = Board.new
+		player1 = Player.new
+		result = true
+		player1.turn = true
+		player1.pick_spot(board)
+		assert_equal(result, board.setup.include?("X"))
+	end
 end
