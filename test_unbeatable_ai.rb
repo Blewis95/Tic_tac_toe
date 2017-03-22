21209cc3ca5f103a012069b9e05ae4b43aac43b2
Brian Lewis <blewis8958@gmail.com>
2017-03-22 12:23:49 -0400

    in the middle of debugging

--- test_unbeatable_ai.rb
+++ test_unbeatable_ai.rb
@@ -3,18 +3,69 @@ require_relative "unbeatable_ai.rb"
 require_relative "board.rb"
 
 class TestUnbeatableAI < Minitest::Test
-	def test_new_ai
-		ai1 = Unbeatable_ai.new
-		result = ["X", "X", "X", "X"]
-		assert_equal(result, ai1.create)
-	end
+	# def test_new_ai
+	# 	ai1 = Unbeatable_ai.new
+	# 	result = ["X", "X", "X", "X"]
+	# 	assert_equal(result, ai1.create)
+	# end
 
-	def test_pick_spot_step_1
+	# def test_pick_spot_step_1
+	# 	board = Board.new
+	# 	ai1 = Unbeatable_ai.new
+	# 	ai1.turn = true
+	# 	board.setup = ["X", "X", " ", " ", " ", " ", " ", " ", " "]
+	# 	result = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+	# 	ai1.pick_spot(board)
+	# 	assert_equal(result, board.setup)
+	# end
+
+	# def test_pick_spot_step_1_2
+	# 	board = Board.new
+	# 	ai1 = Unbeatable_ai.new
+	# 	ai1.turn = true
+	# 	board.setup = [" ", " ", " ", " ", " ", " ", "X", " ", "X"]
+	# 	result = [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
+	# 	ai1.pick_spot(board)
+	# 	assert_equal(result, board.setup)
+	# end
+
+	# def test_pick_spot_step_2
+	# 	board = Board.new
+	# 	ai1 = Unbeatable_ai.new
+	# 	ai1.turn = true
+	# 	board.setup = ["O", " ", "O", " ", " ", "X", " ", " ", "X"]
+	# 	result = ["O", "X", "O", " ", " ", "X", " ", " ", "X"]
+	# 	ai1.pick_spot(board)
+	# 	assert_equal(result, board.setup)
+	# end
+
+	# def test_pick_spot_step_2_2
+	# 	board = Board.new
+	# 	ai1 = Unbeatable_ai.new
+	# 	ai1.turn = true
+	# 	board.setup = [" ", "X", " ", "O", "O", " ", " ", " ", "X"]
+	# 	result = [" ", "X", " ", "O", "O", "X", " ", " ", "X"]
+	# 	ai1.pick_spot(board)
+	# 	assert_equal(result, board.setup)
+	# end
+
+	# def test_pick_spot_step_3
+	# 	board = Board.new
+	# 	ai1 = Unbeatable_ai.new
+	# 	ai1.turn = true
+	# 	board.setup = ["X", " ", " ", " ", "O", " ", "O", " ", "X"]
+	# 	result = ["X", " ", "X", " ", "O", " ", "O", " ", "X"]
+	# 	ai1.pick_spot(board)
+	# 	assert_equal(result, board.setup)
+	# end
+
+	def test_pick_spot_step_3_2
 		board = Board.new
 		ai1 = Unbeatable_ai.new
-		board.setup = ["X", "X", "", "", "", "", "", "", ""]
-		result = ["X", "X", "X", "", "", "", "", "", ""]
+		ai1.turn = true
+		board.setup = [" ", "X", " ", " ", "O", " ", " ", "O", "X"]
+		result = [" ", "X", "X", " ", "O", " ", " ", "O", "X"]
 		ai1.pick_spot(board)
 		assert_equal(result, board.setup)
-	end
+	end	
 end
--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -8,23 +8,29 @@ class Unbeatable_ai
 
 	def pick_spot(board)
 
-		check_win?(board)
+		if @turn == true
+			check_win(board)
+		end
 
-		# if board.check_position?(@counter) == true
+		if @turn == true
+			check_fork(board)
+		end
+
+		print @counter
+
+		if board.check_position?(@counter) == true
 			board.set_position(@counter, @create.pop)
-		# end
+		end
 
 		return board
 
 	end
 
 
-def check_win?(board)
+	def check_win(board) #step 1 (win) and step 2 (block)
 		markers = ["X", "O"]
 
 		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
-		winner = false
 
 		markers.each do |mark|
 			winning.each do |combos|
@@ -38,6 +44,7 @@ def check_win?(board)
 						combos.each do |spot|
 							if (board.setup[spot] != mark)
 								@counter = spot
+								@turn = false
 							end
 						end
 						
@@ -46,20 +53,47 @@ def check_win?(board)
 			end
 		end
 	end
-	# def check_win?(board)
-	# 	winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-	# 	counter = 0
+	
+	def check_fork(board)
+		markers = ["X", "O"]
 
+		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-	# 	winning.each do |option|
-	# 		option.each do |spot|
-	# 			# if board.setup[spot] == "X"
-	# 			# 	counter += 1
-	# 			# 	if counter == 2
-	# 			# 		if board.setup[winning[option].index]
-	# 			# 	end
-	# 			# end
-	# 		end
-	# 	end
-	# end
+		markers.each do |mark|
+			counter_possibility = 0
+			option = Array.new
+			winning.each do |combos|
+				counter_taken = 0
+				counter_empty = 0
+				combos.each do |spot|
+					if (board.setup[spot] == mark)
+						counter_taken += 1
+					elsif (board.setup[spot] == " ")
+						counter_empty += 1
+					end
+				end
+
+				if (counter_taken == 1) && (counter_empty == 2)
+					counter_possibility += 1
+					option.push(combos)
+				end
+			end
+
+			if counter_possibility == 2
+				option.each do |things|
+					# print things
+					# print "\n"
+
+					things.each do |x|
+						if board.setup[x] == " "
+							@counter = x
+							@turn = false
+						end
+					end
+				end
+				# print "\n"
+			end
+		end
+	end
+	
 end
