bc5dd89bb79400d6b8408e91a87803812433370a
Brian Lewis <blewis8958@gmail.com>
2017-03-22 14:03:29 -0400

    have tests for step 3 (fork)

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -6,17 +6,22 @@ class Unbeatable_ai
 		@turn = false
 	end
 
-	def pick_spot(board)
+	def pick_spot(board, player2_marker)
 
 		if @turn == true
-			check_win(board)
+			check_win(board, @create[0])
 		end
 
 		if @turn == true
-			check_fork(board)
+			check_block(board, player2_marker)
 		end
 
-		print @counter
+		if @turn == true
+			check_fork(board, @create[0])
+			
+		end
+
+		# print @counter
 
 		if board.check_position?(@counter) == true
 			board.set_position(@counter, @create.pop)
@@ -27,22 +32,21 @@ class Unbeatable_ai
 	end
 
 
-	def check_win(board) #step 1 (win) and step 2 (block)
-		markers = ["X", "O"]
+	def check_win(board, marker) #step 1 (win)
 
 		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-		markers.each do |mark|
+		
 			winning.each do |combos|
 				matches = 0
 				combos.each do |spot|
-					if board.setup[spot] == mark
+					if board.setup[spot] == marker
 						matches += 1
 					end
 				
 					if matches == 2
 						combos.each do |spot|
-							if (board.setup[spot] != mark)
+							if (board.setup[spot] == " ")
 								@counter = spot
 								@turn = false
 							end
@@ -51,22 +55,44 @@ class Unbeatable_ai
 					end
 				end
 			end
-		end
+	end
+
+	def check_block(board, marker)
+		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+		
+			winning.each do |combos|
+				matches = 0
+				combos.each do |spot|
+					if board.setup[spot] == marker
+						matches += 1
+					end
+				
+					if matches == 2
+						combos.each do |spot|
+							if (board.setup[spot] == " ")
+								@counter = spot
+								@turn = false
+							end
+						end
+						
+					end
+				end
+			end
 	end
 	
-	def check_fork(board)
-		markers = ["X", "O"]
+	def check_fork(board, marker)
 
 		winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-		markers.each do |mark|
+		
 			counter_possibility = 0
 			option = Array.new
 			winning.each do |combos|
 				counter_taken = 0
 				counter_empty = 0
 				combos.each do |spot|
-					if (board.setup[spot] == mark)
+					if (board.setup[spot] == marker)
 						counter_taken += 1
 					elsif (board.setup[spot] == " ")
 						counter_empty += 1
@@ -80,20 +106,14 @@ class Unbeatable_ai
 			end
 
 			if counter_possibility == 2
-				option.each do |things|
-					# print things
-					# print "\n"
 
-					things.each do |x|
-						if board.setup[x] == " "
-							@counter = x
-							@turn = false
-						end
-					end
-				end
-				# print "\n"
+				same = option[0]&option[1]
+				same = same[0]
+				@counter = same
+				@turn = false
+
 			end
-		end
+		
 	end
 	
 end
