4aee9183255f84c9219975667e4580e7cc553495
Brian Lewis <blewis8958@gmail.com>
2017-04-18 13:16:20 -0400

    got the menu page to work other than picture, just having a problem passing values from the select tag

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ get '/' do
 	erb :menu
 end
 
+post '/game' do
+
+	session[:pl1] = params[:player1]
+	session[:pl2] = params[:player2]
+
+	erb :board, :locals => {:pl1 => session[:pl1], :pl2 => session[:pl2]}
+end
+
